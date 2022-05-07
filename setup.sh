6c2c0bc1b77ff2cbfb0c928fc292bbe6ef9f97b8
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-05-07 20:49:02 +0900

    update: fix compose command

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # CaRePi Reader
-./CaRePi_reader/setup.sh
+bash ./CaRePi_reader/setup.sh
 
 # CaRePi API
 cd CaRePi_api
-docker-compose up
+docker-compose up -d
 docker-compose exec rails ./bin/setup
 docker-compose down
 cd ..
