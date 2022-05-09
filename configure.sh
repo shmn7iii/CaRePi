92336759beea4eedfd332de37c13b8e4278b56f3
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-05-09 17:53:10 +0900

    update: sed, one line replace

--- configure.sh
+++ configure.sh
@@ -10,9 +10,9 @@ echo -n "SLACK_CHANNEL: "
 read SLACK_CHANNEL
 
 # Reader
-sed -i -e "s/SLACK_BOT_TOKEN/$SLACK_BOT_TOKEN/" ./CaRePi_reader/config.ini
-sed -i -e "s/SLACK_CHANNEL/$SLACK_CHANNEL/" ./CaRePi_reader/config.ini
+sed -i -e "/slack_bot_token/c slack_bot_token = $SLACK_BOT_TOKEN" ./CaRePi_reader/config.ini
+sed -i -e "/slack_channel/c slack_channel = $SLACK_CHANNEL" ./CaRePi_reader/config.ini
 
 # Slack
-sed -i -e "s/SLACK_BOT_TOKEN/$SLACK_BOT_TOKEN/2" ./CaRePi_slack/.env
-sed -i -e "s/SLACK_APP_TOKEN/$SLACK_APP_TOKEN/2" ./CaRePi_slack/.env
+sed -i -e "/SLACK_BOT_TOKEN/c SLACK_BOT_TOKEN = $SLACK_BOT_TOKEN" ./CaRePi_slack/.env
+sed -i -e "/SLACK_APP_TOKEN/c SLACK_APP_TOKEN = $SLACK_APP_TOKEN" ./CaRePi_slack/.env
