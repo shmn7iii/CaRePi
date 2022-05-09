297283b738ca7b2140cb8e5f9e97bf00ca620821
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2022-05-09 15:42:14 +0900

    fix: typo

--- configure.sh
+++ configure.sh
@@ -10,7 +10,7 @@ echo -n "SLACK_CHANNEL: "
 read SLACK_CHANNEL
 
 # API
-sed -i -e "s/SLACK_BOT_TOKEN/$SLACK_BOT_TOKEN/2" ./CaRePi_api/.env
+sed -i -e "s/SLACK_API_TOKEN/$SLACK_BOT_TOKEN/2" ./CaRePi_api/.env
 sed -i -e "s/SLACK_CHANNEL/$SLACK_CHANNEL/2" ./CaRePi_api/.env
 
 # Slack
